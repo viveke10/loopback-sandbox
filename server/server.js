0f40eb599a12a03be6640a0d4d26c8bf21f78b62
Vivek Pandey <vivek@e10.in>
2015-03-13 10:55:11 +0530

    servejs changes

--- server/server.js
+++ server/server.js
@@ -7,6 +7,26 @@ var app = module.exports = loopback();
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname);
 
+app.use(loopback.token());
+app.use(loopback.context());
+
+app.use(function (req, res, next) {
+  if (!req.accessToken) return next();
+  app.models.User.findById(req.accessToken.userId, function(err, user) {
+    if (err) return next(err);
+    if (!user) return next(new Error('No user with this access token was found.'));
+    res.locals.currentUser = user;
+    //console.log('currentUser: ', user); // voila!
+    var loopbackContext = loopback.getCurrentContext();
+    if (loopbackContext) loopbackContext.set('currentUser', user);
+    var ctx = loopback.getCurrentContext();
+    var currentUser = ctx && ctx.get('currentUser');
+    next();
+  });
+});
+
+
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
